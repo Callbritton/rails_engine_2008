a8ad264225c222dfff88a2ffadbcd79929f6e767
Chris Allbritton <cla.inbox@gmail.com>
2020-12-11 17:51:34 -0500

    Add relationships and validations to models

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,8 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+  has_many :transactions, through: :invoice
+
+  validates_presence_of :quantity
+  validates_presence_of :unit_price
 end
