81ece42f49c186375d9edca4f1e1b937ed7fbbdc
Chris Allbritton <cla.inbox@gmail.com>
2020-12-15 08:11:26 -0500

    Add show and index actions to items controller

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,5 +1,19 @@
 class Api::V1::Items::SearchController < ApplicationController
   def show
-    require "pry"; binding.pry
+    item = Item.find_by("#{attribute} ILIKE '%#{value}%'")
+    render json: ItemSerializer.new(item)
+  end
+
+  def index
+    item = Item.where("#{attribute} ILIKE '%#{value}%'")
+    render json: ItemSerializer.new(item)
+  end
+
+  def attribute
+    params.keys[0]
+  end
+
+  def value
+    params.values[0]
   end
 end
