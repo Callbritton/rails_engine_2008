6776084e4496dcd0cd25c5a3a66f9e93847921ea
Chris Allbritton <cla.inbox@gmail.com>
2020-12-15 08:11:05 -0500

    Add find_all route and index action for items

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       namespace :items do
         get '/:id/merchants', to: 'merchants#show'
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
 
       namespace :merchants do
@@ -14,6 +15,6 @@ Rails.application.routes.draw do
 
       resources :items
       resources :merchants
-  end
+    end
   end
 end
