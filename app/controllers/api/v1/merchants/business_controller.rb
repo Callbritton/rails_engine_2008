120f07c97a6ac109e116a82ff84baa300cb2a392
Chris Allbritton <cla.inbox@gmail.com>
2020-12-18 09:27:42 -0500

    Add revenue_across_range and total_revenue actions to business controller

--- app/controllers/api/v1/merchants/business_controller.rb
+++ app/controllers/api/v1/merchants/business_controller.rb
@@ -6,4 +6,13 @@ class Api::V1::Merchants::BusinessController < ApplicationController
   def most_items
     render json: MerchantSerializer.new(Merchant.most_items(params[:quantity]))
   end
+
+  def revenue_across_range
+    render json: RevenueSerializer.new(Merchant.revenue_across_range(params[:start], params[:end]))
+  end
+
+  def total_revenue
+    merchant = Merchant.find(params[:id])
+    render json: RevenueSerializer.new(merchant.total_revenue)
+  end
 end
